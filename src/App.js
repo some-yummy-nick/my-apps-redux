48d9c224e77d6300c641f184194f5cde41450d99
maratShaimardanov <d9tsDnAc>
2018-07-02 17:18:20 +0300

    Add textarea

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+class Textarea extends Component {
+
+	constructor () {
+		super();
+		this.state = {
+			text: "bob"
+		}
+	}
+
+	setValue=(e)=>{
+		this.setState({
+			text:e.currentTarget.value
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<textarea name="" id="" cols="30" rows="10" value={this.state.text} onChange={this.setValue}></textarea>
+			<div className="score">{this.state.text.length}</div>
+			</div>
+		)
+	}
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+		    <Textarea/>
     );
   }
 }
