c8efef4b058684dec70363b376b386a7485d4ffd
yeffasol <yeffasol@mail.com>
2018-07-09 12:34:33 +0300

    Add react fragment

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </React.Fragment>
     );
   }
 }
